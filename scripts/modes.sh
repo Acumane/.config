ad9f9e0e3deeb76b12f73307596276c53eb61794
Bren <git@bren.page>
2024-10-15 08:27:39 -0400

    fix: Game mode, bindings

--- scripts/modes.sh
+++ scripts/modes.sh
@@ -1,5 +1,5 @@
 SWAP=("capslock = overload(control, esc)" "capslock = C-")
-GAME=$(hyprctl getoption animations:enabled | awk 'NR==2{print $2}')
+GAME=$(hyprctl getoption animations:enabled -j | jq ".int")
 KEYD="$HOME/dotfiles/keyd/global.conf"
 # -k          keys-only mode
 # -k reload   reload keys only (keyd)
@@ -11,28 +11,28 @@ if [ "$2" == "reload" ]; then
 fi
 
 if [[ "$GAME" = 1 ]]; then
-    # ratbagctl "G900" profile active set 1
     sed -i "s/${SWAP[0]}/${SWAP[1]}/" $KEYD
-    # sed -i '/^# Keys (game):/,/^$/{/^#.*:$/! s/^# //}' $KEYD
+    sed -i '/^# Keys (game):/,/^$/{/^#.*:$/! s/^# //}' $KEYD
     sed -i '/^# Mouse (main):/,/^$/{/^$/!{/^#/! s/^/# /}}' $KEYD
     sed -i '/^# Disabled (game):/,/^$/{/^$/!{/^#/! s/^/# /}}' $KEYD
     cp $KEYD /etc/keyd/default.conf && keyd reload
+    ratbagctl "G900" profile 0 led 0 set color ff0f00
     hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword decoration:blur:enabled 0"
 
     [[ "$1" == "-k" ]] && notify-send -i - "Keys switched to game mode" && exit
 
-    liquidctl --match h80i set logo color fixed ff0000 --alert-threshold 75
+    liquidctl --match h80i set logo color blackout --alert-threshold 85
     openrgb -p game.orp
     notify-send -u low -i - "Switched to game mode"
 elif [[ "$GAME" = 0 ]]; then
-    # ratbagctl "G900" profile active set 0
     sed -i "s/${SWAP[1]}/${SWAP[0]}/" $KEYD
     sed -i '/^# Mouse (main):/,/^$/{/^#.*:$/! s/^# //}' $KEYD
-    # sed -i '/^# Keys (game):/,/^$/{/^$/!{/^#/! s/^/# /}}' $KEYD
+    sed -i '/^# Keys (game):/,/^$/{/^$/!{/^#/! s/^/# /}}' $KEYD
     sed -i '/^# Disabled (game):/,/^$/{/^#.*:$/! s/^# //}' $KEYD
     cp $KEYD /etc/keyd/default.conf && keyd reload
+    ratbagctl "G900" profile 0 led 0 set color 6e6482
 
     hyprctl reload
     [[ "$1" == "-k" ]] && notify-send -u low -i - "Keys switched to main mode" && exit
