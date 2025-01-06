170dcfee529fac666d64b9816900ac87a0b30f23
Bren <git@bren.page>
2025-01-05 18:04:13 -0800

    Use hyprlock, misc.

--- scripts/idle-lap.sh
+++ scripts/idle-lap.sh
@@ -13,22 +13,22 @@ ext_dpms_off() {
 
 # User-activated (-f): force standby
 if [ "$1" = "-f" ]; then
-    swaylock --conf="$DOTS/sway/lock.conf" --grace=0 &
+    hyprlock --immediate &
     sleep .5s; brillo -O; brillo -S 0% -u 1000000; blight set 0 & ext_dpms_off
     swayidle -w timeout 1 '' resume "brillo -I -u 100000 && kill \$(pgrep -n swayidle)"
     exit
 fi
 
 # Immediate (-i): lockscreen only
-[ "$1" = "-i" ] && { swaylock --conf="$DOTS/sway/lock.conf" --grace=0 & exit; }
+[ "$1" = "-i" ] && { hyprlock --immediate & exit; }
 
 # Passive (battery): fade (50s), lock (1m30s), backlight off (1m33s), suspend (2m30s)
 swayidle \
 timeout 50 "(acpi -a | grep -q off) && (brillo -O; brillo -S 0% -u 3000000)" resume "pkill brillo; brillo -I -u 150000" \
-timeout 90 "(acpi -a | grep -q off) && (swaylock --conf='$DOTS/sway/lock.conf' --grace-no-mouse & ext_dpms_off)" \
+timeout 90 "(acpi -a | grep -q off) && (hyprlock & ext_dpms_off)" \
 timeout 93 "(acpi -a | grep -q off) && blight set 0" resume "brillo -I -u 150000" \
 timeout 150 "(acpi -a | grep -q off) && systemctl suspend" after-resume "brillo -I -u 150000" \
 timeout 170 "(acpi -a | grep -q on) && (brillo -O; brillo -S 0% -u 3000000)" resume "pkill brillo; brillo -I -u 150000" \
-timeout 210 "(acpi -a | grep -q on) && (swaylock --conf='$DOTS/sway/lock.conf' --grace-no-mouse & ext_dpms_off)" \
+timeout 210 "(acpi -a | grep -q on) && (hyprlock & ext_dpms_off)" \
 timeout 213 "(acpi -a | grep -q on) && blight set 0" resume "brillo -I -u 150000" \
 # Passive (charging): fade (2m50s), lock (3m30s), backlight off (3m33s)
--- scripts/idle.sh
+++ scripts/idle.sh
@@ -4,13 +4,13 @@ exec &> /dev/null
 
 # User-activated (-f): force standby
 if [ "$1" = "-f" ]; then
-    swaylock --conf="$DOTS/sway/lock.conf" --grace=0 &
+    hyprlock --immediate &
     sleep 2 && hyprctl dispatch dpms off
     swayidle -w timeout 1 '' resume "kill \$(pgrep -n swayidle)"
 fi
 
 wayidle -t 60 hyprctl dispatch dpms off & iPID=$!
-swaylock --conf="$DOTS/sway/lock.conf" --grace-no-mouse
+hyprlock
 if [[ $? -eq 0 || $? -eq 2 ]]; then
     kill $iPID
 fi
