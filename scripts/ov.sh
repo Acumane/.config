94d328c1f2c516a057f833f68040b9936229d81c
Bren <git@bren.page>
2025-01-13 19:34:25 -0800

    new ov, use $DEV_TYPE, misc.

--- scripts/ov.sh
+++ scripts/ov.sh
@@ -5,17 +5,14 @@ meta=false; overview=false
 keyd monitor | while read -r line; do
     if [[ $line == *"leftmeta up"* ]]; then
         if [ "$overview" = true ]; then
-            hyprctl dispatch "scroller:toggleoverview"
-            overview=false
             hyprctl reload
+            overview=false
         fi
         meta=false
     elif [[ $line == *"leftmeta down"* ]]; then
         meta=true
     elif [[ $line == *"tab down"* ]] && [ "$meta" = true ] && [ "$overview" = false ]; then
         overview=true
-        hyprctl --batch "\
-            dispatch scroller:toggleoverview;\
-            keyword general:gaps_in 6; keyword general:border_size 4; keyword decoration:rounding 15"
+        hyprctl --batch "keyword general:gaps_in 16; keyword general:gaps_out 64"
     fi
 done
